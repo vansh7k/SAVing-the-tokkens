8f427004c5c250603c1cfd1ab65f0a8cec0cd372
vansh7k <vanshbhadari@gmail.com>
2025-03-20 12:00:51 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const jwt = require('jsonwebtoken');
+
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+  return token;
 };
+
 
 module.exports = encrypt;
